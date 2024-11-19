3aa10ba08858facecf9a45c59d1c56910003c32c
Jenny Seburn <jennifer.seburn@gmail.com>
2024-11-19 14:43:31 -0500

    adjust colors for accessibility

--- src/components/typography/SectionTitle/SectionTitle.classes.ts
+++ src/components/typography/SectionTitle/SectionTitle.classes.ts
@@ -7,14 +7,24 @@ export function useSectionTitleClasses() {
   const root = css({
     fontFamily: theme.font.header.family,
     fontWeight: theme.font.header.weight.semibold,
-    color: theme.color.secondary,
-    "&:small": {
+    color: theme.color.primary,
+    small: {
       display: "block",
       fontFamily: theme.font.standard.family,
       fontWeight: theme.font.standard.weight.light,
+      color: theme.color.fg,
     },
     "&:first-child": {
       marginTop: 0,
+    },
+    "h2&": {
+      color: theme.color.secondary,
+    },
+    "h3&": {
+      color: theme.color.primary,
+    },
+    "h4&": {
+      color: theme.color.accent,
     },
   });
 
--- src/components/typography/SectionTitle/SectionTitle.stories.tsx
+++ src/components/typography/SectionTitle/SectionTitle.stories.tsx
@@ -9,4 +9,45 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Depth2: Story = {
+  args: {
+    title: "H2 Section Title"
+  }
+};
+
+export const Depth2WithSubtitle: Story = {
+  args: {
+    title: "H2 Section Title",
+    subtitle: "H2 Section Subtitle"
+  }
+};
+
+export const Depth3: Story = {
+  args: {
+    title: "H3 Section Title",
+    depth: "3"
+  }
+};
+
+export const Depth3WithSubtitle: Story = {
+  args: {
+    title: "H3 Section Title",
+    subtitle: "H2 Section Subtitle",
+    depth: "3"
+  }
+};
+
+export const Depth4: Story = {
+  args: {
+    title: "H4 Section Title",
+    depth: "4"
+  }
+};
+
+export const Depth4WithSubtitle: Story = {
+  args: {
+    title: "H4 Section Title",
+    subtitle: "H4 Section Subtitle",
+    depth: "4"
+  }
+};
