f025f9b1179ddf46ac5b35702d08f65dae68bd6c
Jenny Seburn <jennifer.seburn@gmail.com>
2024-11-20 11:06:11 -0500

    page not found

--- src/pages/PageNotFound/PageNotFound.classes.ts
+++ src/pages/PageNotFound/PageNotFound.classes.ts
@@ -1,10 +1,38 @@
 import { css } from "@emotion/css";
 import { useTheme } from "@emotion/react";
+import { marginMixin } from "@styles/mixins";
 
 export function usePageNotFoundClasses() {
   const theme = useTheme();
 
-  const root = css({ background: theme.color.muted });
+  const root = css({
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    "& > p": {
+      textAlign: "center",
+    },
+  });
 
-  return { root };
+  const headline = css({
+    fontWeight: theme.font.standard.weight.bold,
+    fontSize: theme.font.standard.size.xlarge,
+  });
+
+  const icon = css({
+    color: theme.color.primary,
+    width: 300,
+    height: "auto",
+    ...marginMixin({ vertical: 32 }),
+  });
+
+  const text = css({
+    color: theme.color.secondary,
+    fontWeight: theme.font.standard.weight.semibold,
+    fontSize: theme.font.standard.size.large,
+    maxWidth: 600,
+  });
+
+  return { root, headline, icon, text };
 }
--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,4 +1,7 @@
+import { cx } from "@emotion/css";
+import { Link } from "@navigation/Link";
 import { usePageNotFoundClasses } from "@pages/PageNotFound";
+import { PageTitle } from "@typography/PageTitle";
 
 /**
  * Page Not Found
@@ -6,5 +9,23 @@ import { usePageNotFoundClasses } from "@pages/PageNotFound";
 export function PageNotFound() {
   const classes = usePageNotFoundClasses();
 
-  return <div className={classes.root}>Page Not Found</div>;
+  return (
+    <>
+      <PageTitle title="Lost Spell" subtitle="404 - Page Not Found" />
+      <div className={classes.root}>
+        <p className={classes.headline}>
+          Oh no! It seems this page has vanished into the ether.
+        </p>
+        <i
+          className={cx("fa-duotone fa-solid fa-book-open-cover", classes.icon)}
+        ></i>
+        <p className={classes.text}>
+          Perhaps a spell went awry, or the page you’re seeking has been
+          spirited away. Fear not!
+          <br />
+          <Link to="/">You can return to the heart of the realm.</Link>
+        </p>
+      </div>
+    </>
+  );
 }
