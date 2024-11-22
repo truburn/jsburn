c1e7d996ecf6ae43e0ffac3da43384b10d0d1956
Jenny Seburn <jennifer.seburn@gmail.com>
2024-11-21 21:45:48 -0500

    remove resume download

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -52,9 +52,7 @@ export function About() {
           <p>
             I'm always excited to collaborate on new projects and tackle
             creative challenges. <Link to="/contact">Reach out</Link>, and let's
-            see what magic we can create! For a summarized version of my
-            expertise and experience, feel free to{" "}
-            <Link to="">download my resume</Link>.
+            see what magic we can create!
           </p>
           <p>
             Want to dive deeper into my world? Discover more on{" "}
